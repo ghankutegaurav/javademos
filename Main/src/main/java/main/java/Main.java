d36c0918a40a7a8bafd52b52dc7c90f54a40a89f
Gaurav Ghankute <gauravghankute@gmail.com>
2022-07-27 17:58:33 +0530

    Added Java demo  application files

---
+++ Main/src/main/java/main/java/Main.java
@@ -0,0 +1,14 @@
+package main.java;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(checkIfInputIsAnEvenNumber(122));
+		// Testing in the main method
+	}
+	
+	public static boolean checkIfInputIsAnEvenNumber(int number) {
+		return number % 2 == 0;
+	}
+}
---
+++ Main/src/main/java/main/java/TestMain.java
@@ -0,0 +1,12 @@
+package main.java;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class TestMain {
+@Test
+public void testInputIsEven(){
+	assertTrue(Main.checkIfInputIsAnEvenNumber(23));
+ }
+ }
